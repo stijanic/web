e0045309ba9687d8672dd6edaa077c46b8aa7a72
stijanic <stijanic@yahoo.fr>
2020-12-20 06:16:43 +0100

    Fun, fun, fun...

--- ClojureScript/src/test/core.cljs
+++ ClojureScript/src/test/core.cljs
@@ -2,9 +2,11 @@
 
 (enable-console-print!)
 
-(defn hello [] "Hello There")
+(defn hello
+  []
+  "Hello There")
 
-(println "Hello worlds in repl!")
+(println "Hello worlds in REPL!")
 
 (js/console.log "Now? When? Cool! Then?")
 (js/console.log "Lovely Leiningen!")
@@ -12,8 +14,14 @@
 (js/console.log "And much more and more!")
 (js/console.log "Sometimes even less than we think that we think!")
 
-(js/console.log (document.getElementsByTagName "p"))
+(js/console.log (document.getElementsByTagName "html"))
 (js/console.log (document.getElementsByTagName "h1"))
+(js/console.log (document.getElementsByTagName "div"))
+
+
+(-> js/document.body.innerHTML (set! "Hello Clojure!!!")) ; Big cleaning!
+
+(js-obj "HTML" "Hello Clojure!!!") ; Sets new content for element
 
 ;; (js/alert "Ready for Fibonacci? Check the console: F12!")
 (defn fib [n]
@@ -22,3 +30,4 @@
 (println (fib 35))
 (println (hello))
 (js/console.log (fib 25))
+(-> js/document.body.innerHTML (set! (fib 25))) ; Big cleaning!
