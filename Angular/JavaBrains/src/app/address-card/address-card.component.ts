e48c41684fdc13d28a602866bfd16384f7401f6c
stijanic <stijanic@yahoo.fr>
2021-02-03 11:35:19 +0100

    Keeping moving with Angular

--- Angular/JavaBrains/src/app/address-card/address-card.component.ts
+++ Angular/JavaBrains/src/app/address-card/address-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-address-card',
@@ -6,23 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./address-card.component.sass']
 })
 export class AddressCardComponent implements OnInit {
-  user: any;
-  @Input("userName") userName!: string;
+  @Input("user") user!: User;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.user = {
-      name: this.userName,
-      title: 'Software Developer',
-      address: '1234 Main St, City, State, 100010',
-      phone: [
-        '123-123-1234',
-        '456-456-4567',
-        '789-789-8790'
-      ]
-    };
   }
 
 }
--- Angular/JavaBrains/src/app/app.component.ts
+++ Angular/JavaBrains/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test';
+  user: User;
+
+  constructor() {
+    this.user = new User();
+    this.user.name = 'Foo Bar';
+    this.user.title = 'Software Developer';
+    this.user.address = '1234 Main St, City, State, 100010';
+    this.user.phone = [
+      '123-123-1234',
+      '456-456-4567',
+      '789-789-8790'
+    ]
+  }
+
 }
