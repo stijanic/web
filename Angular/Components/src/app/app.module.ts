1e2bd23b4a23f86c1899fad2ea15d899d6d46bb1
stijanic <stijanic@yahoo.fr>
2021-02-07 07:16:52 +0100

    Creating a directive

--- Angular/Components/src/app/app.module.ts
+++ Angular/Components/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { TruncatePipe } from './truncate.pipe';
 import { PaginatorComponent } from './paginator/paginator.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PaginatorComponent } from './paginator/paginator.component';
     BlogPostTileComponent,
     BlogListComponent,
     TruncatePipe,
-    PaginatorComponent
+    PaginatorComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
