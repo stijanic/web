1d9a6b31179b0a73d53fd3a5ad30cfaf514bf4dc
stijanic <stijanic@yahoo.fr>
2021-02-11 22:35:45 +0100

    That should be all

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -10,6 +10,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -21,13 +22,14 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-          </Switch>
-          <Switch>
-            <Route exact path="/create">
+            <Route path="/create">
               <Create />
             </Route>
-            <Route exact path="/blogs/:id">
+            <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
